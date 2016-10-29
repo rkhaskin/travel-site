7b225906ebd40c59f1af9ec06b13b050b2f4a902
Slavik <rkhaskin@yahoo.com>
2016-10-29 13:09:47 -0400

    intermediate commit

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename'),
+del = require('del');
+
+var config = {
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
+        css: {
+          template: './gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
+}
+
+gulp.task('beginClean', function(){
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+      .pipe(svgSprite(config))
+      .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySpriteGraphics', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+      .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/*.css')
+      .pipe(rename('_sprite.css'))
+      .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+
+// the tasks run simultaneously. Need to include dependency in copySpriteCSS to prevent it from running before createSprite is complete
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteCSS', 'copySpriteGraphics']);
